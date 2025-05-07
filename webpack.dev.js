a75443a960499c2f6ee5f24d74a4e0d5dc01e587
Ion Drimba Filho <iondrimba@gmail.com>
2025-05-07 20:06:47 -0300

    update project

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,11 +14,10 @@ module.exports = merge(common, {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         use: [
           'style-loader',
           'css-loader',
-          'sass-loader'
         ]
       },
     ]
